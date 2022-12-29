bf47a5c68bcf1adb9c056823c2d758e07d84b8c4
Burapat Nun <93272624+JustDev-08@users.noreply.github.com>
2022-12-29 23:24:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,13 +27,20 @@ app.include_router(insights.router)
 from fastapi.middleware.cors import CORSMiddleware
 
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
 app.add_middleware(
-    CORSMiddleware, allow_origins=["*"],
-    allow_credentials=False,
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
  
 
 @app.get("/", tags=["system"], summary="Redirect to /docs")
